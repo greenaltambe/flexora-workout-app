4d7709bd3decf8afced71346214d8232311ddca7
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-11-16 14:39:36 +0530

    add frontend url in frontend

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:8080",
+	baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:8080",
 	withCredentials: true, // Essential for sending session cookies
 	headers: {
 		"Content-Type": "application/json",
